c118679471ed01912aa04bcd799a8f3f1e64c3d9
yxc <469233417@qq.com>
2020-01-20 16:13:02 +0800

    task plot

--- __main__.py
+++ __main__.py
@@ -10,4 +10,6 @@ if __name__ == '__main__':
     node = Node([5,5,5], 5, 'node')
     node.schedule(Task([3,3,3], 2, 'task1'))
     node.plot([500,500])
+    task = Task([50, 100, 150], 50, 'task')
+    task.plot((1000, 1000))
     print('done')
--- task.py
+++ task.py
@@ -2,6 +2,8 @@
 import os
 import json
 import random
+import numpy as np
+from PIL import Image
 
 class Task(object):
     """Task"""
@@ -10,6 +12,29 @@ class Task(object):
         self.duration = duration
         self.label = label
         self.dimension = len(resources)
+
+    def summary(self, bg_shape=None):
+        if bg_shape is None:
+            bg_shape = (self.duration, max(self.resources))
+        if self.dimension > 0:
+            state_matrices = [np.full(bg_shape, 255, dtype=np.uint8) for i in range(0, self.dimension)]
+            for i in range(0, self.dimension):
+                for row in range(0, self.duration):
+                    for col in range(0, self.resources[i]):
+                        state_matrices[i][row, col] = 0
+            temp = state_matrices[0]
+            for i in range(1, self.dimension):
+                temp = np.concatenate((temp, state_matrices[i]), axis=1)
+            return temp
+        else:
+            return None
+
+    def plot(self, bg_shape=None):
+        if bg_shape is None:
+            bg_shape = (self.duration, max(self.resources))
+        if not os.path.exists('__state__'):
+            os.makedirs('__state__')
+        Image.fromarray(self.summary(bg_shape)).save('__state__/{0}.png'.format(self.label))
 
     def __repr__(self):
         return 'Task(resources={0}, duration={1}, label={2})'.format(self.resources, self.duration, self.label)
