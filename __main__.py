92b08287dceff85397658dab454a0c0f6fd64e6d
yxc <469233417@qq.com>
2020-01-20 17:42:47 +0800

    env plot

--- __main__.py
+++ __main__.py
@@ -5,11 +5,16 @@ from env import load_environment
 
 from task import Task
 from env import Node
+from env import Environment
 
 if __name__ == '__main__':
-    node = Node([5,5,5], 5, 'node')
-    node.schedule(Task([3,3,3], 2, 'task1'))
-    node.plot([500,500])
-    task = Task([50, 100, 150], 50, 'task')
+    node = Node([100,200,300], 20, 'node')
+    node.schedule(Task([30,30,30], 10, 'task1'))
+    node.plot()
+    task = Task([50, 100, 150], 10, 'task')
     task.plot((1000, 1000))
+    environment = Environment([node], 10, 60)
+    environment.queue.append(task)
+    environment.backlog.append(task)
+    environment.plot()
     print('done')
--- env.py
+++ env.py
@@ -4,6 +4,8 @@ import json
 import numpy as np
 from PIL import Image
 
+from task import Task
+
 class Node(object):
     """Node"""
     def __init__(self, resources, duration, label):
@@ -91,10 +93,41 @@ class Environment(object):
         self.queue = []
         self.backlog = []
 
-    def plot(self):
+    def summary(self, bg_shape=None):
+        if bg_shape is None:
+            bg_col = max([max(node.resources) for node in self.nodes])
+            bg_row = max([node.duration for node in self.nodes])
+            bg_shape = (bg_row, bg_col)
+        if len(self.nodes) > 0:
+            dimension = self.nodes[0].dimension
+            temp = self.nodes[0].summary(bg_shape)
+            for i in range(1, len(self.nodes)):
+                temp = np.concatenate((temp, self.nodes[i].summary(bg_shape)), axis=1)
+            for i in range(0, len(self.queue)):
+                temp = np.concatenate((temp, self.queue[i].summary(bg_shape)), axis=1)
+            empty_summary = Task([0]*dimension, 0, 'empty_task').summary(bg_shape)
+            for i in range(len(self.queue), self.queue_size):
+                temp = np.concatenate((temp, empty_summary), axis=1)
+            backlog_summary = empty_summary
+            p_backlog = 0
+            p_row = 0
+            p_col = 0
+            while p_row < bg_shape[0] and p_col < bg_shape[1] and p_backlog < len(self.backlog):
+                backlog_summary[p_row, p_col] = 0
+                p_row = p_row + 1
+                if p_row == bg_shape[0]:
+                    p_row = 0
+                    p_col = p_col + 1
+                p_backlog = p_backlog + 1
+            temp = np.concatenate((temp, backlog_summary), axis=1)
+            return temp
+        else:
+            return None
+
+    def plot(self, bg_shape=None):
         if not os.path.exists('__state__'):
             os.makedirs('__state__')
-        return
+        Image.fromarray(self.summary(bg_shape)).save('__state__/environment.png')
 
     def __repr__(self):
         return 'Environment(nodes={0}, queue={1}, backlog={2})'.format(self.nodes, self.queue, self.backlog)
